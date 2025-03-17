e56a54a6fd06823f810f40bb4bc04638537e9305
dihaxn <ihanlaknuka@gmail.com>
2025-03-17 18:19:51 +0530

    make the interface

---
+++ Google-Chrome-Dinosaur-Game/src/ChromeDinosaur.java
@@ -0,0 +1,20 @@
+package src;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class ChromeDinosaur extends JPanel{
+
+    int boardWidth= 750;
+    int boardHeight= 250;
+
+    public ChromeDinosaur(){
+
+        setPreferredSize(new Dimension(boardWidth,boardHeight));
+        setBackground(Color.lightGray);
+
+    }
+
+
+}
