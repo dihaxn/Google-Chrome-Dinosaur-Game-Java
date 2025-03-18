fd0d87bfce127647e7b39d6f4d82b54d3627eac8
dihaxn <ihanlaknuka@gmail.com>
2025-03-18 19:36:54 +0530

    Import Images

--- Google-Chrome-Dinosaur-Game/src/ChromeDinosaur.java
+++ Google-Chrome-Dinosaur-Game/src/ChromeDinosaur.java
@@ -9,6 +9,15 @@ public class ChromeDinosaur extends JPanel{
     int boardWidth= 750;
     int boardHeight= 250;
 
+
+    //Immage
+    Image dinosaurImg;
+    Image dinosaurDeadImg;
+    Image dinosaurJumping;
+    Image cactus1Img;
+    Image cactus2Img;
+    Image cactus3Img;
+
     public ChromeDinosaur(){
 
         setPreferredSize(new Dimension(boardWidth,boardHeight));
